8841d2b6a1dee7088555d6633240a7b0af893c3e
Dev Goswami <devgirigoswami8@gmail.com>
2026-06-21 16:05:18 -0400

    feat(api): add events and locations controllers

---
+++ server/controllers/locations.js
@@ -0,0 +1,20 @@
+import {pool} from '../config/database.js'
+
+export const getAllLocations = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM locations ORDER BY id ASC')
+        res.status(200).json(result.rows)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+export const getLocation = async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await pool.query('SELECT * FROM locations WHERE id = $1', [id])
+        res.status(200).json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
